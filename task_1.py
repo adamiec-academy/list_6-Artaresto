9d472e8c929885e2ff02a312e71fad7ebe835332
Artaresto <89027893+Artaresto@users.noreply.github.com>
2023-01-24 18:54:34 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,5 +1,18 @@
-from random import shuffle
+def generate_assingments(previous_assingments, coders):
+    
+    my_list = coders.copy()
+    new_ass = previous_assingments.copy()
+    shuffle(my_list)
+    
+    for key, value in previous_assingments.items():
+        while True:
+            if previous_assingments[key] != my_list[0] and previous_assingments[value] != my_list[0] and new_ass.items() != dict(map(reversed, new_ass.items())):   
+                new_ass[key] = my_list[0]
+                my_list.pop(0)
+                break
+            else:
+                shuffle(my_list)
 
-
-def generate_assignments(previous_assignments, coders):
-    return {}
+    return new_ass
+generate_assingments(prev, pop)
+    
