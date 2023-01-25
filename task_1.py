d75c16cd67d2574753a2c3acda2ca161ff6b807d
Artaresto <89027893+Artaresto@users.noreply.github.com>
2023-01-25 19:21:14 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,17 +1,1 @@
-from random import shuffle
 
-def generate_assignments(previous_assignments, coders):
-    mixed_coders = coders.copy()
-    
-    
-    
-    while True:
-        shuffle(mixed_coders) 
-        new_ass = {}
-            
-        for i in range(len(mixed_coders)):
-            new_ass[coders[i]] = mixed_coders[i]
-        
-        if mixed_coders[i] != coders[i] or mixed_coders[i] != coders[mixed_coders.index(coders[i])] and coders[i] in previous_assignments.keys() and previous_assignments[coders[i]] != mixed_coders[i]:
-
-            return new_ass
