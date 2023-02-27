6e14b4563b556f91522f2e1d231cdc78950fff4c
Artaresto <89027893+Artaresto@users.noreply.github.com>
2023-02-27 13:28:00 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -2,6 +2,7 @@ from random import shuffle
 from copy import copy
 
 
+
 def is_self_review(new_assignments):
     for key, value in new_assignments.items():
         if key == value:
@@ -17,7 +18,7 @@ def is_symetric_review(new_assignments):
 
 
 def is_subsequent_review(previous_assignments, new_assignments):
-    for key in new_assignments.items():
+    for key in new_assignments:
         if key in previous_assignments and new_assignments[key] == previous_assignments[key]:
             return True
     return False
@@ -43,15 +44,5 @@ def generate_assignments(previous_assignments, coders):
             new_assignments[coders[i]] = shuffled_coders[i]
 
         if are_assignments_correct(previous_assignments, new_assignments):
-           return new_assignments
-
-
-    while True:
-        shuffle(shuffled_coders)
-        new_assignments = {}
-
-        for i in range(len(coders)):
-            new_assignments[coders[i]] = shuffled_coders[i]
-
-        if are_assignments_correct(previous_assignments, new_assignments):
             return new_assignments
+
